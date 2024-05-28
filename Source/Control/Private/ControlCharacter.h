3e246041d6b7bedc426726ed59b7d6a0f943a385
Reminauld Pijuan <guitarguru767@gmail.com>
2024-05-28 12:40:27 +0100

    Initial Boost Logic | 12:39 | 28.05.24
    Just add velocity

--- Source/Control/Private/ControlCharacter.h
+++ Source/Control/Private/ControlCharacter.h
@@ -120,6 +120,10 @@ private:
 	UFUNCTION()
 	void StopFlying();
 
+	UFUNCTION()
+	void ApplyBoost(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
